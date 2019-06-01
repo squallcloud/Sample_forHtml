f3097f1a7742fa8d677ad66e26e7e609aacfc7ef
squallcloud <squallcloudzidane@gmail.com>
2019-06-01 14:57:50 +0900

    feat: webストレージのサンプル追加

---
+++ Sample_WebStorage/js/script.js
@@ -0,0 +1,22 @@
+﻿function saveLocalStorage() {
+    var array = [];
+    var obj = {
+        'キー1': '値1',
+        'キー2': '値2'
+    };
+    array.push(obj);
+
+    var setjson = JSON.stringify(obj);
+    localStorage.setItem('キー', setjson);
+}
+
+function clearLocalStorage() {
+    localStorage.clear();
+}
+
+function getLocalStorage() {
+    var data = localStorage.getItem('キー');
+    console.log(data);
+    var obj = JSON.parse(data);
+    return obj;
+}
